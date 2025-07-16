66751e13002354bd10a458dcd67a0583df61ef14
Pecky <3148917744@qq.com>
2025-07-16 14:14:22 +0800

    NextWeek

--- include/ray.h
+++ include/ray.h
@@ -7,7 +7,10 @@ class ray {
   public:
     ray() {}
 
-    ray(const point3& origin, const vec3& direction) : orig(origin), dir(direction) {}
+    ray(const point3& origin, const vec3& direction, const double tm) : 
+      orig(origin), dir(direction), tm(tm){}
+      
+    ray(const point3& origin, const vec3& direction) : ray(origin, direction, 0) {}
 
     const point3& origin() const  { return orig; }
     const vec3& direction() const { return dir; }
@@ -16,9 +19,14 @@ class ray {
         return orig + t*dir;
     }
 
+    double time() const {
+        return tm;
+    }
+
   private:
     point3 orig;
     vec3 dir;
+    double tm;
 };
 
 #endif
--- include/sphere.h
+++ include/sphere.h
@@ -6,11 +6,23 @@
 
 class sphere : public hittable {
   public:
-    sphere(const point3& center, double radius, shared_ptr<material> mat)
-      : center(center), radius(std::fmax(0,radius)), mat(mat) {}
+    sphere(const point3& static_center, double radius, shared_ptr<material> mat)
+      : center(static_center, vec3(0, 0, 0)), radius(std::fmax(0,radius)), mat(mat) {
+        vec3 rvec = vec3(radius, radius, radius);
+        bbox = aabb(static_center - rvec, static_center + rvec);
+    }
+
+    sphere(const point3& center_1, const point3& center_2, double radius, shared_ptr<material> mat)
+      : center(center_1, center_2 - center_1), radius(std::fmax(0,radius)), mat(mat) {
+        vec3 rvec = vec3(radius, radius, radius);
+        auto box1 = aabb(center_1 - rvec, center_1 + rvec);
+        auto box2 = aabb(center_2 - rvec, center_2 + rvec);
+        bbox = aabb(box1, box2);
+    }
 
     bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
-        vec3 oc = center - r.origin();
+        point3 center_now = center.at(r.time());
+        vec3 oc = center_now - r.origin();
         auto a = r.direction().length_squared();
         auto h = dot(r.direction(), oc);
         auto c = oc.length_squared() - radius*radius;
@@ -31,17 +43,22 @@ class sphere : public hittable {
 
         rec.t = root;
         rec.p = r.at(rec.t);
-        vec3 outward_normal = (rec.p - center) / radius;
+        vec3 outward_normal = (rec.p - center_now) / radius;
         rec.set_face_normal(r, outward_normal);
         rec.mat = mat;
 
         return true;
     }
 
+    aabb bounding_box() const override {
+      return bbox;
+    }
+
   private:
-    point3 center;
+    ray center;
     double radius;
     shared_ptr<material> mat;
+    aabb bbox;
 };
 
 #endif
