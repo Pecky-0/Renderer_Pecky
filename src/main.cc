3ec14b7651eb585dea6bb3446bf9755e6eb9dfda
Pecky <3148917744@qq.com>
2025-08-29 12:29:30 +0800

    Pecky_Renderer

--- src/main.cc
+++ src/main.cc
@@ -246,8 +246,8 @@ void cornell_box() {
     camera cam;
 
     cam.aspect_ratio      = 1.0;
-    cam.image_width       = 1600;
-    cam.samples_per_pixel = 10000;
+    cam.image_width       = 800;
+    cam.samples_per_pixel = 7000;
     cam.max_depth         = 50;
     cam.background        = color(0,0,0);
 
